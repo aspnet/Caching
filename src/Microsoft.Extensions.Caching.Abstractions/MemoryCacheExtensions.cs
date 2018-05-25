f2d8d5ef4778ed42271babeebbfd2bcbdacdfd22
Tamas Stanitz <tamas.stanitz@logmein.com>
2018-05-25 20:45:14 +0200

    return default when key not exists

--- src/Microsoft.Extensions.Caching.Abstractions/MemoryCacheExtensions.cs
+++ src/Microsoft.Extensions.Caching.Abstractions/MemoryCacheExtensions.cs
@@ -17,7 +17,7 @@ namespace Microsoft.Extensions.Caching.Memory
 
         public static TItem Get<TItem>(this IMemoryCache cache, object key)
         {
-            return (TItem)cache.Get(key);
+            return (TItem)(cache.Get(key) ?? default(TItem));
         }
 
         public static bool TryGetValue<TItem>(this IMemoryCache cache, object key, out TItem value)
